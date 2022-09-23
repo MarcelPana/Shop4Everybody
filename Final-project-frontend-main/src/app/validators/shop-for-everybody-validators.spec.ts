e0088b18c6805bd836bd4ebb3530efae0831aeae
MarcelPana <79997464+MarcelPana@users.noreply.github.com>
2022-09-23 12:12:23 +0200

    first commit

---
+++ Final-project-frontend-main/src/app/validators/shop-for-everybody-validators.spec.ts
@@ -0,0 +1,7 @@
+import { Shop4EverybodyValidators } from './shop-for-everybody-validators';
+
+describe('Shop4EverybodyValidators', () => {
+  it('should create an instance', () => {
+    expect(new Shop4EverybodyValidators()).toBeTruthy();
+  });
+});
